0ff04555661be79d31858f1f2eaab9dde332b3ea
Vidalatan <vidalatan@gmail.com>
2022-04-25 14:23:26 -0500

    Added userSchema

--- models/User.js
+++ models/User.js
@@ -1,9 +1,27 @@
 const { Schema, model } = require('mongoose');
+const thoughtsSchema = require('./Thought')
 
 // Schema to create Student model
 const userSchema = new Schema(
   {
-    // 
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        validator: function(v) {
+          return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+        }
+      }
+    },
+    thoughts: [thoughtsSchema],
+    friends: [this]
   },
   {
     toJSON: {
@@ -11,6 +29,12 @@ const userSchema = new Schema(
     },
   }
 );
+
+userSchema.virtual('friendCount').get(
+  function() {
+    return this.friends.length;
+  }
+)
 
 const Student = model('users', userSchema);
 
