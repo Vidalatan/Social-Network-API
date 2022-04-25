271bbd49ff3285eba085406139f08aac74537d9a
Vidalatan <vidalatan@gmail.com>
2022-04-25 15:11:21 -0500

    Added thoughtRoutes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,4 +2,14 @@ const router = require('express').Router();
 
 const C = require('../../controllers/thoughtController')
 
+router.route('/').get(C.getThoughts)
+
+router.route('/create/:userId').post(C.createThought)
+
+router.route('/:thoughtId').get(C.getThoughtById).put(C.updateThoughtById).delete(C.deleteThoughtById)
+
+router.route('/:thoughtId/reactions').post(C.createReaction)
+
+router.route('/:thoughtId/reactions/:reactionId').post(C.getReactionById)
+
 module.exports = router;
