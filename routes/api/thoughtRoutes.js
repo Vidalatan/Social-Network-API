aa48a00f100e1c5fde9fc91dc5056a25bda8aa1d
Vidalatan <vidalatan@gmail.com>
2022-04-25 15:05:18 -0500

    updated userRoutes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+const C = require('../../controllers/thoughtController')
+
+module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,11 @@
 const router = require('express').Router();
 
+const C = require('../../controllers/userController')
+
+router.route('/').get(C.getUsers).post(C.createUser)
+
+router.route('/:userId').get(C.getUserById).put(C.updateUserById).delete(C.deleteUserById)
+
+router.route('/:userId/friends/:friendId').post(C.addFriendById).delete(C.deleteFriendById)
+
 module.exports = router;
